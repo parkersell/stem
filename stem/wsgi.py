54f09aece41a2651f848a7c7f304cc60473d4907
parkersell <parkersell27@gmail.com>
2020-06-23 14:26:08 -0400

    Update wsgi.py

--- stem/wsgi.py
+++ stem/wsgi.py
@@ -1,16 +1,29 @@
-"""
-WSGI config for stem project.
+# This file contains the WSGI configuration required to serve up your
+# web application at http://threedp12.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# +++++++++++ GENERAL DEBUGGING TIPS +++++++++++
+# getting imports and sys.path right can be fiddly!
+# We've tried to collect some general tips here:
+# https://help.pythonanywhere.com/pages/DebuggingImportError
 
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
 
+
+# +++++++++++ DJANGO +++++++++++
+# To use your own django app use code like this:
 import os
-
+import sys
+#
+## assuming your django settings file is at '/home/threedp12/mysite/mysite/settings.py'
+## and your manage.py is is at '/home/threedp12/mysite/manage.py'
+path = '/home/threedp12/stem/stem/stem'
+if path not in sys.path:
+    sys.path.append(path)
+#
+os.environ['DJANGO_SETTINGS_MODULE'] = 'stem.settings'
+#
+## then:
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stem.settings')
-
 application = get_wsgi_application()
